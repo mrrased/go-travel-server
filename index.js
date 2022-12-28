53982513efe1b5aec9ad151ad27918ba946e7afa
mrrased <rashedtechdr.786@gmail.com>
2022-12-28 22:07:56 +0600

    added user and password

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const fileUpload = require('express-fileupload')
 
 
 app.get('/', (req, res) => res.send('Go travel!'))
+
+// passWord: 8UBTkaXTRNwBU54U
+// UserName: go-travel
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zingp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log(uri);
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
